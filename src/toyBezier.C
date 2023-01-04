91bed51139b5d5d6973b6f712e8a2579ba5c26fc
Margaret Lazarovits <mlazarovits@gmail.com>
2023-01-04 15:47:03 -0600

    cleaned up

--- src/toyBezier.C
+++ src/toyBezier.C
@@ -19,11 +19,11 @@ int main(int argc, char *argv[]){
 	string fname = "test.root";
 	bool viz = false;
 	unsigned long long seed = 123;
-	int nDataPts = 3;
+	int nDataPts = 1;
 	double mu = 0.5;
 	double sigma = 0.25;
 	double xmax = 1;
-	double xmin = -1;
+	double xmin = 0;
 	bool hprint = false;
 	for(int i = 0; i < argc; i++){
 		if(strncmp(argv[i],"--help", 6) == 0){
@@ -79,7 +79,7 @@ int main(int argc, char *argv[]){
    		cout << "   --help(-h)          print options" << endl;
    		cout << "   --ouput(-o) [file]  output root file (in test/)" << endl;
    		cout << "   --viz       	turns on visualization" << endl;
-   		cout << "   --nDataPts(-n) [n]  sets number of data points/control points" << endl;
+   		cout << "   --nDataPts(-n) [n]  sets number of data points/control points (order of Bezier curve)" << endl;
    		cout << "   --mu                sets mean of Gaussian to sample data from" << endl;
    		cout << "   --sigma             sets sigma of Gaussian to sample data from" << endl;
    		cout << "   --xmax              sets x-maximum of Gaussian to sample data from (xmax > xmin)" << endl;
@@ -91,53 +91,62 @@ int main(int argc, char *argv[]){
 	
 	cout << "Hi welcome to Chili's!" << endl;
 	
+//set intermediate (x,r) points, but (0,0) and (1,1) are set
+	nDataPts += 2;
+	int nSamples = 1000;//number of samples for t NOT including endpoints and x_global and r_global
 	double x[nDataPts];
+	double r[nDataPts];
+	double x_global[nSamples];
+	double r_global[nSamples];
+	TGraph* gr;
 
 //simulate data for control points	
 	RandomSample rs(seed);
 	rs.SetRange(xmin,xmax);
-	rs.SampleGaussian(mu, sigma, nDataPts, x);
-	cout << "sampled data" << endl;
-	//set r coord for simulated data for plotting
-	double r_data[nDataPts];
-	for(int i = 1; i < nDataPts+1; i++) r_data[i] = float(i)/float(nDataPts); 
-//sort sim x for plotting
-	std::sort(x,x+nDataPts);
-	int nSamples = 1000;//number of samples for r and Bezier curve NOT including endpoints
-	double x_approx[nSamples+1]; //filled by reference in CalculateCurve
-	double r[nSamples+1];
-	BezierCurve bc(x, nDataPts, r, nSamples);
-	bc.CalculateCurve(x_approx);
-	cout << "calculated Bezier curve" << endl;
-	//skip first r point (r = 0) and last point (r = 1)
-	double deriv[nSamples];
-	NumDeriv derivate(r,x_approx,nSamples);
-	derivate.MapToInterval(0.,1.);
-	derivate.FiniteDiff(deriv);
-	double r_deriv[nSamples];
-	cout << "calculated derivative" << endl;
+	rs.SampleGaussian(mu, sigma, nSamples, x_global);
+	rs.SampleGaussian(mu,sigma, nSamples, r_global);
+	
+	
+	//test SelectPoints
+	rs.SelectPoints(x_global, nSamples, x, nDataPts);
+	rs.SelectPoints(r_global, nSamples, r, nDataPts);
+	x[0] = 0.;
+	r[0] = 0.;
+	x[nDataPts-1] = 1.;
+	r[nDataPts-1] = 1.;
+	for(int i = 0; i < nDataPts; i++) cout << i << " x: " << x[i] << " r: " << r[i] << endl;
+	//sort sim x for plotting
+	//std::sort(x,x+nDataPts); - don't need to since sampling from larger population
+
 	
 
-//cout << "last 10 approximation points" << endl;	
-//	for(int i = 0; i < nSamples; i++) cout << "point #" << i << ": x_approx: " << x_approx[i] << " r: " << r[i] << " derivative: " << deriv[i] << endl;
-	if(viz){
+
+	//x and r are functions of t
+	double x_approx[nSamples+1]; //filled by reference in CalculateCurve
+	double r_approx[nSamples+1]; //filled by reference in CalculateCurve
+	double t[nSamples+1];
+	BezierCurve x_bc(x, nDataPts, t, nSamples);
+	BezierCurve r_bc(r, nDataPts, t, nSamples);
+	x_bc.CalculateCurve(x_approx);
+	r_bc.CalculateCurve(r_approx);
+	cout << "calculated Bezier curves for x and r - order " << nDataPts-2 << endl;
+
+
+	if(viz){	
+		gr = new TGraph(nSamples+1, r_approx, x_approx);
+		
+		gr->SetLineWidth(1);
+		gr->SetLineColor(kRed-7);
+		gr->SetMarkerStyle(15);
+
 		cout << "Plots stored in: " << "test/" << fname << endl;
 		TFile* f = TFile::Open(("test/"+fname).c_str(),"RECREATE");
-		TGraph* gr_approx = new TGraph(nSamples+1, r, x_approx);
-		TGraph* gr_data = new TGraph(nDataPts, r_data, x);
-		TGraph* gr_deriv = new TGraph(nSamples, r, deriv);
+		TGraph* gr_data = new TGraph(nDataPts, r, x);
 		TLine* line = new TLine(0,0,1,1);
 		TMultiGraph* mg = new TMultiGraph();		
 		TLegend* leg = new TLegend(0.54,0.23,0.89,0.35);
 		TCanvas* cv = new TCanvas("cv");
        	
-       		
-		gr_approx->SetLineWidth(1);
-		gr_approx->SetLineColor(kRed-7);
-		gr_approx->SetMarkerStyle(15);
-		gr_deriv->SetLineWidth(1);
-		gr_deriv->SetLineColor(kBlue-7);
-		gr_deriv->SetMarkerStyle(15);
 
 
 		gr_data->SetMarkerSize(0.95);
@@ -146,11 +155,10 @@ int main(int argc, char *argv[]){
        		gr_data->SetMarkerColor(kViolet+7);
 		
 		mg->Add(gr_data);
+		mg->Add(gr);
+		
 		leg->AddEntry(gr_data, "Original data/control points");
-		mg->Add(gr_approx);
-		leg->AddEntry(gr_approx,"Bezier Approximation");
-		mg->Add(gr_deriv);
-		leg->AddEntry(gr_deriv,"Derivative of Bezier Approximation");
+		leg->AddEntry(gr,"3rd order Bezier approximation");
 		
 		cv->cd();
 		cv->SetRightMargin(0.09);
@@ -171,7 +179,7 @@ int main(int argc, char *argv[]){
 		l.SetNDC();
 		l.SetTextAlign(11);
 		l.SetTextSize(0.03);
-		l.DrawLatex(0.18,0.92,"Data as Control Points for Bezier Curve and Derivative in Coord-Rank Plane");
+		l.DrawLatex(0.18,0.92,"Bezier Curves to Independently Approximate x and r");
 		
 		f->cd();
 		cv->Write();
